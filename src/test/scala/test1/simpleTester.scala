be71c7a2e0738c1129411085eb629c04fa0f11a8
yashi0524 <ajno5@msn.com>
2022-05-25 15:40:52 +0800

    Add time cross   vcd file

--- src/test/scala/test1/simpleTester.scala
+++ src/test/scala/test1/simpleTester.scala
@@ -3,6 +3,7 @@ import chiseltest._
 import chiselverify.coverage.CoverageReporter
 import chiselverify.coverage._
 import chiselverify.coverage.{cover => ccover}
+import chiselverify.timing._
 import org.scalatest.flatspec.AnyFlatSpec
 import simpleModule._
 
@@ -21,14 +22,20 @@ class simpleTester extends AnyFlatSpec with ChiselScalatestTester {
                 bin("lo10", 0 to 10),
                 bin("First100", 0 to 100)
             ),
-            ccover("in_b", dut.io.in_b)(DefaultBin(dut.io.in_b))
+            ccover("in_b", dut.io.in_b)(DefaultBin(dut.io.in_b)),
+            ccover("time cross", dut.io.in_a, dut.io.out_a)(Exactly(1))(
+            cross("both1", Seq(1 to 1, 1 to 1))
+                )
         )
         
         def testSingle( a:UInt, b:UInt, c:UInt ): Unit ={
             dut.io.in_a.poke(a)
             dut.io.in_b.poke(b)
             dut.io.in_c.poke(c)
-            dut.clock.step(1)        
+            dut.clock.step(1) 
+            
+            cr.sample()
+            cr.step()
         }
         
         for( a <- 0 until 10){
@@ -36,7 +43,7 @@ class simpleTester extends AnyFlatSpec with ChiselScalatestTester {
                 testSingle( a.U, b.U, 1.U)
         }
         
-        cr.sample()
+        
         
         cr.printReport()
     }
